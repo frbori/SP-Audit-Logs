106638c1dd9e35f8bcce502c4920421bfd36d621
Francesco Bori <frbori@microsoft.com>
2022-05-03 18:17:57 +0200

    -

--- GetSPAuditLogs/run.ps1
+++ GetSPAuditLogs/run.ps1
@@ -10,94 +10,119 @@ if (-not $name) {
 } #>
 
 #region Parameters
+# Environment Variables
 $keyVaultName = "keyvaultspauditlogs"
 $keyVaultCertName = "mycert"
 $appId = "62d46db3-d611-4781-93f6-5a1b8f0c5360"
 $tenant = "M365x798487.onmicrosoft.com"
-$siteUrl = "https://M365x798487.sharepoint.com"
+
+# Request Body Parameters
+<# REQUEST BODY
+    $start         # dd/MM/yyyy
+    $end           # dd/MM/yyyy
+    $operations    # Operation1,Operation2,OperationN
+    $siteUrl       # https:\\xxx
+    docLibUrl      # https:\\yyy 
+#>
+$startDay = "02/05/2022"
+$endDay = "02/05/2022"
+$operations = @("PageViewed", "FileAccessed")
+$siteUrl = "https://m365x798487.sharepoint.com"
+$docLibName = "Docs"
 #endregion Parameters
 
-#region Connections
-$keyVaultCertificateSecret = Get-AzKeyVaultSecret -VaultName $keyVaultName -Name $keyVaultCertName -AsPlainText
-$certificate = New-Object -TypeName System.Security.Cryptography.X509Certificates.X509Certificate2 -ArgumentList ([Convert]::FromBase64String( $keyVaultCertificateSecret )), '', 'Exportable,MachineKeySet,PersistKeySet'
-
-Import-Module -Name "PnP.PowerShell" -RequiredVersion "1.10.0"
-Connect-PnPOnline -ClientId $appId -Tenant $tenant -Url $siteUrl -CertificateBase64Encoded $keyVaultCertificateSecret
-
-Import-Module -Name "ExchangeOnlineManagement" -RequiredVersion "2.0.5"
-Connect-ExchangeOnline -AppID $appId -Organization $tenant -Certificate $certificate
-#endregion Connections
-
-#Get-Location    # C:\home\site\wwwroot
-
-#region Script temp
-[DateTime]$start = [DateTime]::UtcNow.AddHours(-24)
-[DateTime]$end = [DateTime]::UtcNow
-
-$nowTime = [DateTime]::UtcNow.ToString("ddMMyyyy hhmmss")
-$outputFile = "$($(Get-Location).Path)\AuditLogRecords_$nowTime.csv"
-$record = "SharePoint"
-$resultSize = 5000
-$intervalMinutes = 60
-[DateTime]$currentStart = $start
-[DateTime]$currentEnd = $start
-
-Write-Host "Retrieving audit records for the date range between $($start) and $($end), RecordType=$record, ResultsSize=$resultSize"
-
+$errorMsg = $null
+$scriptStackTrace = $null
+$statusCode = [HttpStatusCode]::OK
 $totalCount = 0
-while ($true) {
-    $currentEnd = $currentStart.AddMinutes($intervalMinutes)
-    if ($currentEnd -gt $end) {
-        $currentEnd = $end
-    }
 
-    if ($currentStart -eq $currentEnd) {
-        break
-    }
+try {
+    #region Connections
+    $keyVaultCertificateSecret = Get-AzKeyVaultSecret -VaultName $keyVaultName -Name $keyVaultCertName -AsPlainText
+    $certificate = New-Object -TypeName System.Security.Cryptography.X509Certificates.X509Certificate2 -ArgumentList ([Convert]::FromBase64String( $keyVaultCertificateSecret )), '', 'Exportable,MachineKeySet,PersistKeySet'
 
-    $sessionID = [Guid]::NewGuid().ToString() + "_" + "ExtractLogs" + (Get-Date).ToString("yyyyMMddHHmmssfff")
-    Write-Host "Retrieving audit records for activities performed between $($currentStart) and $($currentEnd)"
-    $currentCount = 0
+    Import-Module -Name "PnP.PowerShell" -RequiredVersion "1.10.0"
+    Connect-PnPOnline -ClientId $appId -Tenant $tenant -Url $siteUrl -CertificateBase64Encoded $keyVaultCertificateSecret -ErrorAction Stop
 
-    do {
-        $results = Search-UnifiedAuditLog -StartDate $currentStart -EndDate $currentEnd -SessionId $sessionID -SessionCommand ReturnLargeSet -ResultSize $resultSize -ObjectIds "$siteUrl/*" -Operations PageViewed
+    Import-Module -Name "ExchangeOnlineManagement" -RequiredVersion "2.0.5"
+    Connect-ExchangeOnline -AppID $appId -Organization $tenant -Certificate $certificate
+    #endregion Connections
+
+    #region Script
+    [DateTime]$start = Get-Date -Year $startDay.Split("/")[2] -Month $startDay.Split("/")[1] -Day $startDay.Split("/")[0] -Hour 0 -Minute 0 -Second 0
+    [DateTime]$end = Get-Date -Year $endDay.Split("/")[2] -Month $endDay.Split("/")[1] -Day $endDay.Split("/")[0] -Hour 0 -Minute 0 -Second 0
+    $end = $end.AddHours(23).AddMinutes(59).AddSeconds(59)
+    $nowTime = [DateTime]::UtcNow.ToString("ddMMyyyy hhmmss")
+    $outputFile = "$($(Get-Location).Path)\AuditLogRecords_$nowTime.csv"
+    $record = "SharePoint"
+    $resultSize = 5000
+    $intervalMinutes = 720
+    [DateTime]$currentStart = $start
+    [DateTime]$currentEnd = $start
+
+    $site = get-pnpsite -Includes Id -ErrorAction Stop
+
+    Write-Host "Retrieving audit records for the date range between $($start) and $($end), RecordType=$record, ResultsSize=$resultSize"
+
+    while ($true) {
+        $currentEnd = $currentStart.AddMinutes($intervalMinutes)
+        if ($currentEnd -gt $end) {
+            $currentEnd = $end
+        }
+
+        if ($currentStart -eq $currentEnd) {
+            break
+        }
+
+        $sessionID = [Guid]::NewGuid().ToString() + "_" + "ExtractLogs" + (Get-Date).ToString("yyyyMMddHHmmssfff")
+        Write-Host "Retrieving audit records for activities performed between $($currentStart) and $($currentEnd)"
+    
+        do {
+            $results = Search-UnifiedAuditLog -StartDate $currentStart -EndDate $currentEnd -SessionId $sessionID -SessionCommand ReturnLargeSet -ResultSize $resultSize -Operations $operations -SiteIds $site.id
         
-        if (($results | Measure-Object).Count -ne 0) {
-            $results | export-csv -Path $outputFile -Append -NoTypeInformation    
-                        
-            foreach ($record in $results) {
-                $json = ConvertFrom-Json $record.AuditData                
-                
-                Write-Host "User:" $record.UserIds     -ForegroundColor DarkCyan
-                Write-Host "On:" $record.CreationDate  -ForegroundColor DarkCyan
-                Write-Host "Visited:" $json.ObjectId   -ForegroundColor DarkCyan 
-                Write-host "----"
-            }
+            if (($results | Measure-Object).Count -ne 0) {
+                      
+                foreach ($record in $results) {
+                    $json = ConvertFrom-Json $record.AuditData                
+                    Add-Member -InputObject $record -MemberType NoteProperty -Name "ObjectId" -Value $json.ObjectId
+                }
 
-            $currentTotal = $results[0].ResultCount
-            $totalCount += $results.Count
-            $currentCount += $results.Count
+                $results | export-csv -Path $outputFile -Append -NoTypeInformation
+
+                $currentTotal = $results[0].ResultCount
+                $totalCount += $results.Count
             
-            if ($currentTotal -eq $results[$results.Count - 1].ResultIndex) {
-                Write-Host "Successfully retrieved $($currentTotal) audit records for the current time range. Moving on to the next interval." -foregroundColor Yellow
-                break
+                if ($currentTotal -eq $results[$results.Count - 1].ResultIndex) {
+                    Write-Host "Successfully retrieved $($currentTotal) audit records for the current time range. Moving on to the next interval." -foregroundColor Yellow
+                    break
+                }
             }
         }
+        while (($results | Measure-Object).Count -ne 0)
+
+        $currentStart = $currentEnd
     }
-    while (($results | Measure-Object).Count -ne 0)
+    Write-Host "Finished retrieving audit records for the date range between $($start) and $($end). Total count: $totalCount" -foregroundColor Green
 
-    $currentStart = $currentEnd
+    if (Test-Path -Path $outputFile) {
+        Add-PnPFile -Path $outputFile -Folder $docLibName -ErrorAction Stop
+        Remove-Item -Path $outputFile
+    }
+    #endregion Script
+
+    #region Disconnections
+    Disconnect-ExchangeOnline -Confirm:$false
+    Disconnect-PnPOnline
+    #endregion Disconnections
 }
-Write-Host "Script complete! Finished retrieving audit records for the date range between $($start) and $($end). Total count: $totalCount" -foregroundColor Green
-#endregion Script temp
-
-#region Disconnections
-Disconnect-ExchangeOnline -Confirm:$false
-Disconnect-PnPOnline
-#endregion Disconnections
+catch {
+    $errorMsg = $_.Exception.Message
+    $scriptStackTrace = $_.ScriptStackTrace
+    $statusCode = [HttpStatusCode]::InternalServerError
+}
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = [HttpStatusCode]::OK
-    Body = "Ok"
-})
+        StatusCode = $statusCode
+        Body       = "{""RecordsRetrieved"" = ""$totalCount"", ""ErrorMsg"" = ""$errorMsg"",""ScriptStackTrace"" = ""$scriptStackTrace""}"
+    })
